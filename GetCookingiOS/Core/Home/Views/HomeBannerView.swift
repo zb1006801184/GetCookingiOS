9c5cf1d78ec2d8cbcde8bd02fcc024134b74d306
zzzz <www.1006801184@qq.com>
2025-09-19 18:59:18 +0800

    feat: 新增首页轮播图组件及相关图片资源，优化首页展示效果

---
+++ GetCookingiOS/Core/Home/Views/HomeBannerView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeBannerView.swift
+//  GetCookingiOS
+//
+//  Created by zhubiao07 on 2025/9/19.
+//
+
+import SwiftUI
+
+struct HomeBannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeBannerView()
+}
